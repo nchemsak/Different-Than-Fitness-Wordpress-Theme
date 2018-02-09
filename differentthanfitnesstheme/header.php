ba4096bdd6dbe2fc67a854d61e6384195c3bd8fb
Nick <nchemsak@gmail.com>
2018-02-09 12:07:35 -0600

    header

--- differentthanfitnesstheme/header.php
+++ differentthanfitnesstheme/header.php
@@ -4,70 +4,54 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title> 
+    <title>
         <?php wp_title(); ?>
     </title>
     <?php wp_head(); ?>
-</head> 
+</head>
 
-<body id="body"> 
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12">
+<body id="body">
+    <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+    <!--BELOW IS AN EXAMPLE USING THE BOOTSTRAP NAVWALKER MENU: wp_bootstrap_navwalker.php-->
+    <div id="all-headings" class="content-page-heading-wrapper">
+        <div class="large-banner-wrapper">
+            <div id="sticky-nav-wrapper" class="navbar-fixed-top">
+                <div id="black-banner-section">
+                    <a href="/"><img src="/wp-content/themes/differentthanfitnesstheme/images/DT-logo-3.png" class="img-responsive dtf-logo" id="dtf-logo-contentpage-id" alt="Different Than Fitness - Nashville TN" /></a>
+                    <h2 id="dtf-heading-text">DIFFERENT THAN FITNESS</h2>
                 </div>
+              <header class="navbar navbar-default bs-docs-nav" role="banner">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                            <?php
+                                if(has_nav_menu('main-menu')){
+                                    wp_nav_menu(array(
+                                        'theme_location'  => 'main-menu',
+                                        'container'       => false, 
+                                        'menu_class'      => 'nav navbar-nav', 
+                                        'menu_id'         => 'main-menu',
+                                        'echo'            => true,
+                                        'fallback_cb'     => 'wp_page_menu',
+                                        'before'          => '',
+                                        'after'           => '',
+                                        'link_before'     => '',
+                                        'link_after'      => '',
+                                        'depth'           => 0,
+                                        'walker'          => new wp_bootstrap_navwalker()
+                                    ));
+                                }
+                            ?>
+                        </nav>
+                    </div>
+                </header>
             </div>
         </div>
-    </header> 
-
-
-
-
-
-<!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////// --> 
-<!--BELOW IS AN EXAMPLE USING THE BOOTSTRAP NAVWALKER MENU: wp_bootstrap_navwalker.php-->
-
-
-
-<!--
-	<header class="navbar navbar-default navbar-fixed-top bs-docs-nav" role="banner">
-    <div class="container">
-        <div class="navbar-header">
-            <img src="wp-content/themes/lomonaco/images/Logo.png" class="img-responsive hidden-sm hidden-md hidden-lg navbar-logo" />
-                       
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-           
-           
-           <?php
-                    if(has_nav_menu('main-menu')){
-                         wp_nav_menu(array(
-                            'theme_location'  => 'main-menu',
-                            'container'       => false, 
-                            'menu_class'      => 'nav navbar-nav', 
-                            'menu_id'         => 'main-menu',
-                            'echo'            => true,
-                            'fallback_cb'     => 'wp_page_menu',
-                            'before'          => '',
-                            'after'           => '',
-                            'link_before'     => '',
-                            'link_after'      => '',
-                            'depth'           => 0,
-                            'walker'          => new wp_bootstrap_navwalker()
-                         ));
-                    }
-				?>
-           
-           
-
-        </nav>
     </div>
-</header>
--->
